6c25d1fa00e60419c4ab6eb8b6ae779a817ed21a
a20sarle <a20sarle.student@his.se>
2021-04-13 17:24:35 +0200

    Button and EditText retrieved

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //button
+        Button id_button = (Button) findViewById(R.id.id_button);//get id of button
+        id_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "You've clicked the button", Toast.LENGTH_LONG).show();//display the text of button
+            }
+        });
     }
 }
+
 
 
