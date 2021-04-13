12fd828a809f21432c10cf6d5f4ce204e5ccaa2d
a20sarle <a20sarle.student@his.se>
2021-04-13 15:31:14 +0200

    Button and LinearLayout changed

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.Toast;
 import android.view.View;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,16 +18,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //button
-        Button id_button = (Button) findViewById(R.id.id_button);//get id of button
-        id_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "You've clicked the button", Toast.LENGTH_LONG).show();//display the text of button
-            }
-        });
+            final EditText MyEditText = (EditText) findViewById(R.id.edit_text);//get the id for edit text
+            Button displayText = (Button) findViewById(R.id.id_button);//get the id for button
+            displayText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (MyEditText.getText().toString() != null)//check whether the entered text is not null
+                    {
+                        Toast.makeText(getApplicationContext(), MyEditText.getText().toString(), Toast.LENGTH_LONG).show();//display the text that you entered in edit text
+                    }
+                }
+            });
+        }
 
-    }
+        //button
+        //Button id_button = (Button) findViewById(R.id.id_button);//get id of button
+        //id_button.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        Toast.makeText(getApplicationContext(), "You've clicked the button", Toast.LENGTH_LONG).show();//display the text of button
+        //    }
+        //});
 }
 
 
